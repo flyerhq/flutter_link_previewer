d1062db2a21605298a791cf148b0e3a6c0898059
Alex Demchenko <alexdemchenko@yahoo.com>
2021-09-10 00:23:45 +0200

    Code cleanup

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
       home: MyHomePage(),
     );
   }
